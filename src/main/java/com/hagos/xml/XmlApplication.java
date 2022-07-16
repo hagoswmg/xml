3c22c6c2c94442f01a308ffa264e2d6f885aa31a
hagoswmg <getachew.hagos@wmg.com>
2022-07-15 17:52:14 -0700

    Externalized db_connection parameters

--- src/main/java/com/hagos/xml/XmlApplication.java
+++ src/main/java/com/hagos/xml/XmlApplication.java
@@ -7,9 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XmlApplication {
 
 	public static void main(String[] args) {
-		final String APPLICATION_PORT = System.getenv("PORT");
 		SpringApplication.run(XmlApplication.class, args);
+		String APPLICATION_PORT = System.getenv("PORT");
+
 		System.out.println("APPLICATION_PORT = " + APPLICATION_PORT);
+		System.out.println("DB_URL = " + System.getenv("DB_URL"));
+		System.out.println("DB_USERNAME = " + System.getenv("DB_USERNAME"));
+		System.out.println("DB_PASSWORD = " + System.getenv("DB_PASSWORD"));
 	}
 
 }
